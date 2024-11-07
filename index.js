e2d0527596c00985383b84eaf6965962aa3532cc
gihoon <kiki01111@naver.com>
2024-11-07 23:46:18 +0900

    Fix: heroku buildpacks:clear

--- index.js
+++ index.js
@@ -16,96 +16,111 @@ var socketIo = require("socket.io");
 var app = express();
 var server = http.createServer(app);
 var io = socketIo(server, {
-    cors: {
-        origin: "https://lymming.link/",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "https://lymming.link/",
+    methods: ["GET", "POST"],
+  },
 });
 var totalRooms = {};
 io.on("connection", function (socket) {
-    console.log("Client connected. socket: ".concat(socket.id));
-    socket.on("join", function (data) {
-        if (!(data === null || data === void 0 ? void 0 : data.room))
-            return;
-        socket.join(data.room);
-        if (!totalRooms[data.room]) {
-            totalRooms[data.room] = { users: [], ready: new Set() };
+  console.log("Client connected. socket: ".concat(socket.id));
+  socket.on("join", function (data) {
+    if (!(data === null || data === void 0 ? void 0 : data.room)) return;
+    socket.join(data.room);
+    if (!totalRooms[data.room]) {
+      totalRooms[data.room] = { users: [], ready: new Set() };
+    }
+    totalRooms[data.room].users.push(socket.id);
+    socket.room = data.room;
+    console.log("Join room ".concat(data.room, ". Socket ").concat(socket.id));
+  });
+  socket.on("ready", function () {
+    var room = socket.room;
+    if (!room || !totalRooms[room]) return;
+    //사용자 ready 상태에 추가
+    //!
+    totalRooms[room].ready.add(socket.id);
+    console.log("Socket ".concat(socket.id, "is ready in room ").concat(room));
+    console.log("".concat(totalRooms[room].users));
+    console.log("룸 크기", totalRooms[room].ready.size);
+    console.log("----end ready log----");
+    //모든 사용자 ready 상태인지 확인
+    if (totalRooms[room].ready.size === 2) {
+      console.log(totalRooms[room]);
+      console.log("#All users in room ".concat(room, " are ready"));
+      //모든 사용자 ready상태일 경우 allReady상태 전송
+      io.to(room).emit("allReady");
+    }
+  });
+  socket.on("offer", function (data) {
+    console.log("offer");
+    socket.to(data.room).emit("offer", { sdp: data.sdp, sender: socket.id });
+  });
+  socket.on("answer", function (data) {
+    console.log("answer");
+    socket.to(data.room).emit("answer", { sdp: data.sdp, sender: socket.id });
+  });
+  socket.on("candidate", function (data) {
+    console.log("candidate");
+    socket
+      .to(data.room)
+      .emit("candidate", { candidate: data.candidate, sender: socket.id });
+  });
+  socket.on("screenSharing", function (data) {
+    var room = data.room,
+      isScreenSharing = data.isScreenSharing;
+    console.log(
+      "\uBC29 "
+        .concat(room, "\uC5D0\uC11C \uD654\uBA74\uACF5\uC720\uC0C1\uD0DC: ")
+        .concat(isScreenSharing)
+    );
+    socket
+      .to(room)
+      .emit("screenSharing", {
+        isScreenSharing: isScreenSharing,
+        sender: socket.id,
+      });
+  });
+  socket.on("callEnded", function (msg) {
+    socket.to(msg.room).emit("callEnded");
+  });
+  // dddd
+  socket.on("disconnect", function () {
+    if (socket.room && totalRooms[socket.room]) {
+      totalRooms[socket.room].users = totalRooms[socket.room].users.filter(
+        function (id) {
+          return id !== socket.id;
         }
-        totalRooms[data.room].users.push(socket.id);
-        socket.room = data.room;
-        console.log("Join room ".concat(data.room, ". Socket ").concat(socket.id));
+      );
+      totalRooms[socket.room].ready["delete"](socket.id);
+    }
+    if (totalRooms[socket.room] && totalRooms[socket.room].users.length === 0) {
+      delete totalRooms[socket.room];
+    }
+    console.log("Client disconnected");
+  });
+  socket.on("toggleMic", function (data) {
+    console.log(
+      "User ".concat(data.userId, " toggled mic: ").concat(data.isMicOn)
+    );
+    socket.to(data.room).emit("toggleMic", {
+      room: data.room,
+      userId: data.userId,
+      isMicOn: data.isMicOn,
     });
-    socket.on("ready", function () {
-        var room = socket.room;
-        if (!room || !totalRooms[room])
-            return;
-        //사용자 ready 상태에 추가
-        //!
-        totalRooms[room].ready.add(socket.id);
-        console.log("Socket ".concat(socket.id, "is ready in room ").concat(room));
-        console.log("".concat(totalRooms[room].users));
-        console.log("룸 크기", totalRooms[room].ready.size);
-        console.log("----end ready log----");
-        //모든 사용자 ready 상태인지 확인
-        if (totalRooms[room].ready.size === 2) {
-            console.log(totalRooms[room]);
-            console.log("#All users in room ".concat(room, " are ready"));
-            //모든 사용자 ready상태일 경우 allReady상태 전송
-            io.to(room).emit("allReady");
-        }
+  });
+  socket.on("toggleVideo", function (data) {
+    console.log(
+      "User ".concat(data.userId, " toggled mic: ").concat(data.isVideoOn)
+    );
+    socket.to(data.room).emit("toggleVideo", {
+      room: data.room,
+      userId: data.userId,
+      isVideoOn: data.isVideoOn,
     });
-    socket.on("offer", function (data) {
-        console.log("offer");
-        socket.to(data.room).emit("offer", { sdp: data.sdp, sender: socket.id });
-    });
-    socket.on("answer", function (data) {
-        console.log("answer");
-        socket.to(data.room).emit("answer", { sdp: data.sdp, sender: socket.id });
-    });
-    socket.on("candidate", function (data) {
-        console.log("candidate");
-        socket
-            .to(data.room)
-            .emit("candidate", { candidate: data.candidate, sender: socket.id });
-    });
-    socket.on("screenSharing", function (data) {
-        var room = data.room, isScreenSharing = data.isScreenSharing;
-        console.log("\uBC29 ".concat(room, "\uC5D0\uC11C \uD654\uBA74\uACF5\uC720\uC0C1\uD0DC: ").concat(isScreenSharing));
-        socket
-            .to(room)
-            .emit("screenSharing", { isScreenSharing: isScreenSharing, sender: socket.id });
-    });
-    socket.on("callEnded", function (msg) {
-        socket.to(msg.room).emit("callEnded");
-    });
-    socket.on("disconnect", function () {
-        if (socket.room && totalRooms[socket.room]) {
-            totalRooms[socket.room].users = totalRooms[socket.room].users.filter(function (id) { return id !== socket.id; });
-            totalRooms[socket.room].ready["delete"](socket.id);
-        }
-        if (totalRooms[socket.room] && totalRooms[socket.room].users.length === 0) {
-            delete totalRooms[socket.room];
-        }
-        console.log("Client disconnected");
-    });
-    socket.on("toggleMic", function (data) {
-        console.log("User ".concat(data.userId, " toggled mic: ").concat(data.isMicOn));
-        socket.to(data.room).emit("toggleMic", {
-            room: data.room,
-            userId: data.userId,
-            isMicOn: data.isMicOn
-        });
-    });
-    socket.on("toggleVideo", function (data) {
-        console.log("User ".concat(data.userId, " toggled mic: ").concat(data.isVideoOn));
-        socket.to(data.room).emit("toggleVideo", {
-            room: data.room,
-            userId: data.userId,
-            isVideoOn: data.isVideoOn
-        });
-    });
+  });
 });
 var PORT = process.env.PORT || 8008;
 server.listen(PORT, function () {
-    console.log("Listening on port ".concat(PORT));
+  console.log("Listening on port ".concat(PORT));
 });
